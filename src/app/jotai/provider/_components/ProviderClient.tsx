55e7b6bf18b4ebae43808c6e3b405a74bba242c0
maeta187 <nexus18789@gmail.com>
2023-12-24 15:44:30 +0900

    💄 : ProviderClient コンポーネントの作成

---
+++ src/app/jotai/provider/_components/ProviderClient.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import UseProvider from '@/app/jotai/provider/_components/UseProvider'
+
+const UseAtomsClient = () => {
+  return (
+    <div>
+      <UseProvider />
+    </div>
+  )
+}
+
+export default UseAtomsClient
